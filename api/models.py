e60f796da1f94be1828bb34cbfa79dbdf7510529
egamberdiyevtursunboy <egamberdiyevtursunboy@gmail.com>
2023-06-03 12:46:21 +0500

    api

---
+++ api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class BotUser(models.Model):
+    user_id = models.CharField(max_length=120)
+    name = models.CharField(max_length=120)
+    username = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+    
+class Feedback(models.Model):
+    user_id = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    body =  models.CharField(max_length=120, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
